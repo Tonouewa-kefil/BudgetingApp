68ddf4da4267321b96ac574f36a78728c5126fb4
Tonouewa-kefil <tonouewakefil@gmail.com>
2024-12-01 19:15:52 -0500

    got the two textfield displaying on the same line.

--- budgeting_app/lib/features/authentication/presentation/pages/sign_up_page.dart
+++ budgeting_app/lib/features/authentication/presentation/pages/sign_up_page.dart
@@ -1,9 +1,11 @@
 import 'package:budgeting_app/core/theme/app_colors.dart';
+import 'package:budgeting_app/features/authentication/presentation/pages/sign_in_page.dart';
 import 'package:budgeting_app/features/authentication/presentation/widgets/authentication_textfield.dart';
-import 'package:budgeting_app/features/authentication/presentation/widgets/authetication_button.dart';
+import 'package:budgeting_app/features/authentication/presentation/widgets/authentication_button.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => const SignUpPage());
   const SignUpPage({super.key});
 
   @override
@@ -30,6 +32,7 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
@@ -44,18 +47,32 @@ class _SignUpPageState extends State<SignUpPage> {
               const SizedBox(
                 height: 50,
               ),
-              AuthenticationTextfield(
-                hintText: "First Name",
-                obscure: false,
-                controller: firstNameController,
+              SizedBox(
+                height: 75,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: (MediaQuery.sizeOf(context).width - (16 + 15)) / 2,
+                      child: AuthenticationTextfield(
+                        hintText: "First Name",
+                        obscure: false,
+                        controller: firstNameController,
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    SizedBox(
+                      width: (MediaQuery.sizeOf(context).width - (16 + 15)) / 2,
+                      child: AuthenticationTextfield(
+                        hintText: "Last Name",
+                        obscure: false,
+                        controller: lastNameController,
+                      ),
+                    )
+                  ],
+                ),
               ),
-              const SizedBox(height: 10),
-              AuthenticationTextfield(
-                //
-                hintText: "Last Name", obscure: false,
-                controller: lastNameController,
-              ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 5),
               AuthenticationTextfield(
                 hintText: "Email",
                 obscure: false,
@@ -80,20 +97,25 @@ class _SignUpPageState extends State<SignUpPage> {
               const SizedBox(
                 height: 20,
               ),
-              RichText(
-                text: TextSpan(
-                    text: 'Already have an Account? ',
-                    style: Theme.of(context).textTheme.titleMedium,
-                    children: [
-                      TextSpan(
-                          text: "Sign In",
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleMedium
-                              ?.copyWith(
-                                  color: AppColor.sblue,
-                                  fontWeight: FontWeight.bold))
-                    ]),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, SignInPage.route());
+                },
+                child: RichText(
+                  text: TextSpan(
+                      text: 'Already have an Account? ',
+                      style: Theme.of(context).textTheme.titleMedium,
+                      children: [
+                        TextSpan(
+                            text: "Sign In",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(
+                                    color: AppColor.sblue,
+                                    fontWeight: FontWeight.bold))
+                      ]),
+                ),
               )
             ],
           ),
